3e90331231084568ce7b0741d1b784c73f8c1a96
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-06-27 15:32:38 +0530

    Async Function

--- Day2.js
+++ Day2.js
@@ -1,7 +1,25 @@
 // setTimeout()  --->  Async Function (It is for set time for any function for calling)
 
+/*
 function greetAnyone() {
     console.log("Hello Good Morning !!")
 }
 
 setTimeout(greetAnyone, 5000)
+*/
+
+function findSum(num) {
+    let ans = 0
+    for(let i = 0; i < num; i++) {
+        ans += i;
+    }
+
+    console.log("The Sum of 1 to 100 is : " + ans)
+}
+
+function findSum100() {
+    findSum(100)
+}
+
+setTimeout(findSum100, 5000)
+
